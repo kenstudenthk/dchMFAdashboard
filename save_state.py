4f915fdd38b1136dd5e1b530f1b4c4eee08f038f
kenstudenthk <kenstudenthk@yahoo.com.hk>
2025-04-11 20:28:27 +0800

    Update

--- save_state.py
+++ save_state.py
@@ -5,7 +5,7 @@ from typing import List
 # Azure AD Configuration
 CLIENT_ID = "b3eee569-7d4b-4976-9af4-9f683063448f"
 CLIENT_SECRET = "oVL8Q~5Xe4y~86pIBYPBOUi7swmIsRpnj75aAcGy"
-TENANT_ID = "0c354a30-f421-4d42-bd98-0d86e396d207"
+TENANT_ID = "common"
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 
 # Redirect URI for authentication
